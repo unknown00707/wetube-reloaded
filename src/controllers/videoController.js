dbd684134a71044db05097893a9dd0475a2088e0
willer <parkminjun0723@gmail.com>
2025-05-22 12:43:59 +0900

    Set base_pug
    set base.pu

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send('Search');
 
-export const see = (req, res) => res.send('Watch');
-export const edit = (req, res) => res.send('Edit');
+export const see = (req, res) => res.render('Watch');
+export const edit = (req, res) => res.render('Edit');
 export const upload = (req, res) => res.send('Upload');
 export const deleteVideo = (req, res) => res.send('Delete Video');
